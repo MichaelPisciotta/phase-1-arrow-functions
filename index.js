8c871e92b72faf4db0e5102bab4e8df9a635229d
MichaelPisciotta <mfp56@miami.edu>
2021-07-21 17:31:39 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+const divide = (num1, num2) => [num1, num2]
+divide(2000,100)
+
+
+
+let x = 2
+const square = x.map(x => x ** 2)
+square()
+
+
+
+
+
+const add = (num1, num2) => {
+    return num1 + num2
+}
+add(3, 4)
